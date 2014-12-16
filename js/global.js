85fbd7186f177cd9b9760e7f5a72538a18295ec6
darthniwo1 <mniwo@hotmail.com>
2014-12-15 21:38:53 -0500

    added modifications to all pages and added new carousel

--- js/global.js
+++ js/global.js
@@ -2,22 +2,51 @@
 
 $(document).ready(function() {
              
-    var owl = $("#owl-demo");
+   /* var owl = $("#owl-demo");
           owl.owlCarousel({
-        items : 5, //10 items above 1000px browser width
-        itemsDesktop : [1000,5], //5 items between 1000px and 901px
-        itemsDesktopSmall : [900,3], // betweem 900px and 601px
-        itemsTablet: [600,2], //2 items between 600 and 0
-        itemsMobile : false // itemsMobile disabled - inherit from itemsTablet option
-      });
+          items : 5, //10 items above 1000px browser width
+          itemsDesktop : [1000,5], //5 items between 1000px and 901px
+          itemsDesktopSmall : [900,3], // betweem 900px and 601px
+          itemsTablet: [600,2], //2 items between 600 and 0
+          itemsMobile : false // itemsMobile disabled - inherit from itemsTablet option
+        });
 
     // Para que el carousel se mueva cuando cargue la pagina 
       owl.trigger('owl.play',1800); //mil es un segundo
       
+      var owl2 =  $("#owl-demo2");
+          owl2.owlCarousel({
+          items : 5, //10 items above 1000px browser width
+          itemsDesktop : [1000,5], //5 items between 1000px and 901px
+          itemsDesktopSmall : [900,3], // betweem 900px and 601px
+          itemsTablet: [600,2], //2 items between 600 and 0
+          itemsMobile : false // itemsMobile disabled - inherit from itemsTablet option
+        });
+           owl2.trigger('owl.play',1800);*/
+
+    $("#owl-demo").owlCarousel({
+          items : 5, //10 items above 1000px browser width
+          itemsDesktop : [1000,5], //5 items between 1000px and 901px
+          itemsDesktopSmall : [900,3], // betweem 900px and 601px
+          itemsTablet: [600,2], //2 items between 600 and 0
+          itemsMobile : false, // itemsMobile disabled - inherit from itemsTablet option
+          autoPlay: 2000
+        });
+     $("#owl-demo2").owlCarousel({
+          items : 8, //10 items above 1000px browser width
+          itemsDesktop : [1000,5], //5 items between 1000px and 901px
+          itemsDesktopSmall : [900,3], // betweem 900px and 601px
+          itemsTablet: [600,2], //2 items between 600 and 0
+          itemsMobile : false, // itemsMobile disabled - inherit from itemsTablet option
+          autoPlay: 3000
+        });
 
     if($('.js-tile-gallery').length){
       $('.tile').magnificPopup({ 
-        type: 'image'
+        type: 'image',
+        gallery:{
+          enabled:true
+        }
         // other options
       });
     }
